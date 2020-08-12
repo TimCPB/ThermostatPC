145f2d099834d10bfd69d57660fa5a83bf002de4
Tim <TimCPB@github.com>
2020-08-12 16:40:49 +0100

    First test in ThermostatSpec.js

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,11 @@
+describe("Thermostat", function() {
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it("Starts at 20 degrees", function() {
+    expect(thermostat.currentTemperature).toEqual(20);
+  });
+});
