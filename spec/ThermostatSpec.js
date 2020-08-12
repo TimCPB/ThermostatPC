d6be8fc1a1545dad2a67fe2318a3f5a095cda3b7
Tim <TimCPB@github.com>
2020-08-12 17:17:10 +0100

    refactor defaultTemperature to currentTemperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -6,30 +6,30 @@ describe("Thermostat", function() {
   });
 
   it("Starts at 20 degrees", function() {
-    expect(thermostat.defaultTemperature).toEqual(20);
+    expect(thermostat.currentTemperature).toEqual(20);
   });
 
   it ("can increase the temperature", function() {
     thermostat.up()
-    expect(thermostat.defaultTemperature).toEqual(21);
+    expect(thermostat.currentTemperature).toEqual(21);
   });
 
   it ("can decrease the temperature", function() {
     thermostat.down()
-    expect(thermostat.defaultTemperature).toEqual(19);
+    expect(thermostat.currentTemperature).toEqual(19);
   });
 
   it ("has a min temperture of 10", function() {
     for(let i = 0; i < 11; i++){
     thermostat.down()
     }
-    expect(thermostat.defaultTemperature).toEqual(10);
+    expect(thermostat.currentTemperature).toEqual(10);
   });
 
   it ("power saving mode prevents temp rise above 25", function(){
     for(let i = 0; i < 6; i++){
       thermostat.up()
     }
-    expect(thermostat.defaultTemperature).toEqual(25);
+    expect(thermostat.currentTemperature).toEqual(25);
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,19 +1,19 @@
 class Thermostat {
   constructor(){
-    this.defaultTemperature = 20
+    this.currentTemperature = 20
     this.minimumTemp = 10
     this.powerSaveModeOn = 25
   };
 
   up(){
-    if(this.defaultTemperature < this.powerSaveModeOn){
-    this.defaultTemperature++;
+    if(this.currentTemperature < this.powerSaveModeOn){
+    this.currentTemperature++;
     }
   }
 
   down(){
-    if(this.defaultTemperature > this.minimumTemp){
-    this.defaultTemperature--;
+    if(this.currentTemperature > this.minimumTemp){
+    this.currentTemperature--;
     }
   }
 }
