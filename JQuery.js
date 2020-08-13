70d5ab7f31749a82f6ddd15469c69ad6a12bfda8
Tim <TimCPB@github.com>
2020-08-13 11:39:30 +0100

    Created JQuery.js with code to manipulate DOM

---
+++ JQuery.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+  $( "#temperature-up" ).click(function( event ) {
+      alert( "It's getting warmer!" );
+  });
+  $( "#temperature-down" ).click(function( event ) {
+      alert( "It's getting cooler!" );
+  });
+ });
